42bfda3336ffee62d1b749a472bb5234c9b3a497
RAHUL NAIR <134130649+RahulNair15@users.noreply.github.com>
2024-07-07 21:37:47 +0530

    Add files via upload

---
+++ Quiz Application/js/questions.js
@@ -0,0 +1,114 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+     {
+     numb: 6,
+    question: "In how many ways can CSS be written in?",
+     answer: "3",
+    options: [
+       "1",
+       "2",
+       "3",
+       "4"
+    ]
+   },
+     {
+    numb: 7,
+   question: "What type of CSS is generally recommended for designing large web pages?",
+    answer: "External",
+   options: [
+      "Inline",
+      "Internal",
+      "External",
+      "None of the above"
+   ]
+  },
+    {
+    numb: 8,
+   question: "How can we specify the spacing between each letter in a text in CSS?",
+    answer: "letter-spacing",
+   options: [
+      "alpha-spacing",
+      "letter-spacing",
+      "character-spacing",
+      "None of the above"
+   ]
+  },
+    {
+    numb: 9,
+   question: "Can negative values be allowed in padding property?",
+    answer: "No",
+   options: [
+      "Yes",
+      "No",
+      "Depends on property",
+      "None of the above"
+   ]
+  },
+    {
+    numb: 10,
+   question: "The CSS property used to specify the transparency of an element is?",
+    answer: "opacity",
+   options: [
+      "opacity",
+      "visibility",
+      "filter",
+      "None of the above"
+   ]
+  },
+];
